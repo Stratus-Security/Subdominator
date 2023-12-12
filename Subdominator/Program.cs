a6b3b08f52b091fcf6774326209248ab9a165dbf
coj337 <cojwatson@gmail.com>
2023-12-12 13:32:09 +1100

    Add non-zero exit code when takeovers are found for better DevOps support

--- Subdominator/Program.cs
+++ Subdominator/Program.cs
@@ -54,7 +54,7 @@ public class Program
         {
             rawDomains.Add(o.Domain);
         }
-        else 
+        else
         {
             throw new Exception("A domain (-d) or file (-l) must be specified.");
         }
@@ -62,6 +62,7 @@ public class Program
         // Define maximum concurrent tasks
         int maxConcurrentTasks = o.Threads;
         bool verbose = o.Verbose;
+        bool isAnyVulnerable = false;
 
         // Pre-check domains passed in and filter any that are invalid
         var domains = FilterAndNormalizeDomains(rawDomains);
@@ -91,7 +92,11 @@ public class Program
 
         await Parallel.ForEachAsync(domains, new ParallelOptions { MaxDegreeOfParallelism = maxConcurrentTasks }, async (domain, cancellationToken) =>
         {
-            await CheckAndLogDomain(hijackChecker, domain, o.OutputFile, verbose);
+            var isVulnerable = await CheckAndLogDomain(hijackChecker, domain, o.OutputFile, verbose);
+            if(isVulnerable)
+            {
+                isAnyVulnerable = true;
+            }
             Interlocked.Increment(ref completedTasks);
         });
 
@@ -104,6 +109,12 @@ public class Program
         var rate = completedTasks / elapsed.TotalSeconds;
         Console.WriteLine($"{completedTasks}/{domains.Count()} domains processed. Average rate: {rate:F2} domains/sec");
         Console.WriteLine("Done in " + stopwatch.Elapsed.TotalSeconds + "s");
+
+        // Exit non-zero if we have a takeover, for the DevOps folks
+        if (isAnyVulnerable)
+        {
+            Environment.ExitCode = 1;
+        }
     }
 
     static async Task PeriodicUpdateAsync(TimeSpan interval, Action action, CancellationToken cancellationToken)
@@ -146,17 +157,22 @@ public class Program
         return normalizedDomains.Where(d => d.IsValid).Select(d => d.Original);
     }
 
-    static async Task CheckAndLogDomain(SubdomainHijack hijackChecker, string domain, string outputFile, bool verbose = false)
+    static async Task<bool> CheckAndLogDomain(SubdomainHijack hijackChecker, string domain, string outputFile, bool verbose = false)
     {
+        bool isFound = false;
         try
         {
             var (isVulnerable, fingerprint, cnames) = await hijackChecker.IsDomainVulnerable(domain);
             if (isVulnerable || verbose)
             {
+                if (isVulnerable)
+                {
+                    isFound = true;
+                }
                 var fingerPrintName = fingerprint == null ? "-" : fingerprint.Service;
 
                 // Build the output string
-                var output = $"[{fingerPrintName}] {domain}";
+                var output = $"[{fingerPrintName}] {domain} - CNAME: {string.Join(", ", cnames)}";
 
                 // Thread-safe console print and append to results file
                 lock (_fileLock)
@@ -165,7 +181,7 @@ public class Program
                     Console.ForegroundColor = isVulnerable ? ConsoleColor.Red : ConsoleColor.Green;
                     Console.Write(fingerPrintName);
                     Console.ResetColor();
-                    Console.Write($"] {domain}\n");
+                    Console.Write($"] {domain} - CNAME: {string.Join(", ", cnames)}" + Environment.NewLine);
 
                     if (!string.IsNullOrWhiteSpace(outputFile))
                     {
@@ -174,9 +190,10 @@ public class Program
                 }
             }
         }
-        catch (Exception ex) 
+        catch (Exception ex)
         {
             Console.WriteLine(ex.ToString());
         }
+        return isFound;
     }
 }
